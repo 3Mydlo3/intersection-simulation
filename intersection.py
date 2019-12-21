bb2f3c1de21efceedd8cc5936cf3313c9574a9bb
3Mydlo3 <mydlo333@interia.pl>
2019-12-21 19:13:58 +0100

    Create intersection lanes list

--- intersection.py
+++ intersection.py
@@ -9,3 +9,17 @@ class Intersection(ObjectBase):
             Road(id_=1, parent_object=self),
             Road(id_=2, parent_object=self)
         ]
+        self.incoming_lanes = self.get_incoming_lanes()
+        self.outgoing_lanes = self.get_outgoing_lanes()
+
+    def get_incoming_lanes(self):
+        incoming_lanes = []
+        for road in self.roads:
+            incoming_lanes.append(road.get_incoming_lane())
+        return incoming_lanes
+
+    def get_outgoing_lanes(self):
+        outgoing_lanes = []
+        for road in self.roads:
+            outgoing_lanes.append(road.get_outgoing_lane())
+        return outgoing_lanes
