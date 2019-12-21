032a1e6484e5d41809ad727cda4f16930a6715bf
3Mydlo3 <mydlo333@interia.pl>
2019-12-21 19:17:52 +0100

    Create streams
    They are created for incoming lanes (2 streams per lane) and are connected to outgoing lanes of remaining roads

--- intersection.py
+++ intersection.py
@@ -1,4 +1,5 @@
 from road import Road
+from stream import Stream
 from objectBase import ObjectBase
 
 class Intersection(ObjectBase):
@@ -11,6 +12,7 @@ class Intersection(ObjectBase):
         ]
         self.incoming_lanes = self.get_incoming_lanes()
         self.outgoing_lanes = self.get_outgoing_lanes()
+        self.streams = self.initialize_streams()
 
     def get_incoming_lanes(self):
         incoming_lanes = []
@@ -23,3 +25,20 @@ class Intersection(ObjectBase):
         for road in self.roads:
             outgoing_lanes.append(road.get_outgoing_lane())
         return outgoing_lanes
+
+    def get_streams(self):
+        return self.streams
+
+    def initialize_streams(self):
+        """Create streams and initialize their priorities"""
+        streams = []
+        # Create streams
+        _i = 0
+        for _x in range (0, 3):
+            for _y in range (0, 3):
+                if (_x != _y):
+                    stream = Stream(id_=_i, parent_object=self.roads[_x].get_incoming_lane(),
+                                    connected_with=self.roads[_y].get_outgoing_lane())
+                    streams.append(stream)
+                    _i += 1
+        return streams
