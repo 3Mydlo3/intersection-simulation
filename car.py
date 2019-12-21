5b9526f51e57553902d201152e50a23bec8a2bb5
3Mydlo3 <mydlo333@interia.pl>
2019-12-21 19:46:02 +0100

    Create lane queue

--- car.py
+++ car.py
@@ -8,3 +8,7 @@ class Car(ObjectBase):
     """
     def __init__(self, id_=None, parent_object=None):
         super().__init__(id_=id_, parent_object=parent_object)
+        self.move_to_queue()
+
+    def move_to_queue(self):
+        self.parent_object.add_to_queue(self)
--- lane.py
+++ lane.py
@@ -4,6 +4,10 @@ class Lane(ObjectBase):
     def __init__(self, id_=None, parent_object=None, outgoing=False):
         super().__init__(id_=id_, parent_object=parent_object)
         self.outgoing = outgoing
+        self.queue = []
 
     def is_incoming(self):
         return (False if self.outgoing else True)
+
+    def add_to_queue(self, object_):
+        self.queue.append(object_)
