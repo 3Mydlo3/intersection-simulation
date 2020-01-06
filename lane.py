7a34e30a7d4a083b4cd94374b5e1ae682f014681
3Mydlo3 <mydlo333@interia.pl>
2020-01-06 21:16:05 +0100

    Add `Lane.get_first_car()` and `Lane.remove_first_car()`
    First one only returns car, second one also removes it from queue.

--- lane.py
+++ lane.py
@@ -10,4 +10,21 @@ class Lane(ObjectBase):
         return (False if self.outgoing else True)
 
     def add_to_queue(self, object_):
+        """Method adds object (car) to queue"""
         self.queue.append(object_)
+
+    def get_first_car(self):
+        """Method returns first car from queue or None if not availabe."""
+        try:
+            car = self.queue[0]
+            return car
+        except IndexError:
+            return None
+
+    def remove_first_car(self):
+        """Method removes and returns first car from queue or None if not available."""
+        try:
+            car = self.queue.pop(0)
+            return car
+        except IndexError:
+            return None
