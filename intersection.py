c8bdfe9921258d0a8fb2bed2054722038a361ef9
3Mydlo3 <mydlo333@interia.pl>
2020-01-21 18:12:13 +0100

    `ObjectBase` add `get_current_time()`

--- intersection.py
+++ intersection.py
@@ -4,8 +4,8 @@ from priority import Priority
 from objectBase import ObjectBase
 
 class Intersection(ObjectBase):
-    def __init__(self):
-        super().__init__(parent_object=None)
+    def __init__(self, parent_object=None):
+        super().__init__(parent_object=parent_object)
         self.roads = [
             Road(id_=0, parent_object=self),
             Road(id_=1, parent_object=self),
