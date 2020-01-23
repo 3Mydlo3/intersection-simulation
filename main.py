08ce23d684bf2b5db84e431d6daa4cec9a0d1077
3Mydlo3 <mydlo333@interia.pl>
2020-01-24 00:20:36 +0100

    Create automatic simulation script
    Simulates both variants (with and without lights) 3 times each and gets average for each of them. Does this 100 times and then takes average from all results.

--- main.py
+++ main.py
@@ -2,19 +2,36 @@
 import numpy as np
 # Files
 from simulation import Simulation
+from time_class import Time
 
 """
     SCRIPT
 """
 
-# Simulation parameters
-expected_interval = [np.random.uniform(low=5.0, high=10.0) for _i in range (0, 6)]
-lights_enabled = True
-green_duration = [np.random.randint(low=10, high=20) for _i in range (0, 3)]
+# Simulation results list
+simulations_results = {
+    'lights_enabled': [],
+    'lights_disabled': []
+}
 
-# Create and run simulation
-simulation = Simulation(expected_interval, lights_enabled, green_duration)
-simulation.print_stats()
-# And second time, with lights switched
-simulation = Simulation(expected_interval, not lights_enabled, green_duration)
-simulation.print_stats()
+for _i in range(0, 100):
+    # Simulation parameters
+    expected_interval = [np.random.randint(low=50.0, high=300.0)/10 for _i in range (0, 6)]
+    lights_enabled = True
+    green_duration = [np.random.randint(low=15, high=45) for _i in range (0, 3)]
+    simulation_results_lights_enabled = []
+    simulation_results_lights_disabled = []
+    # Repeat 3 times for average
+    for _j in range (0, 3):
+        # Create and run simulation
+        simulation = Simulation(expected_interval, lights_enabled, green_duration)
+        simulation_results_lights_enabled.append(simulation.get_results()['avg_time_in_system'].convert_to_seconds())
+        # And second time, with lights switched
+        simulation = Simulation(expected_interval, not lights_enabled, green_duration)
+        simulation_results_lights_disabled.append(simulation.get_results()['avg_time_in_system'].convert_to_seconds())
+        print(f"{_i} : {_j}")
+    simulations_results['lights_enabled'].append(np.average(np.array(simulation_results_lights_enabled)))
+    simulations_results['lights_disabled'].append(np.average(np.array(simulation_results_lights_disabled)))
+
+print(f"Average time in system with lights   : {Time.convert_to_time(seconds=np.average(np.array(simulations_results['lights_enabled']))).convert_to_text()}")
+print(f"Average time in system without lights: {Time.convert_to_time(seconds=np.average(np.array(simulations_results['lights_disabled']))).convert_to_text()}")
