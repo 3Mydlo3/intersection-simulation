fb1ba7ce6ea1545b4f1c2ccfcc15348e3a110008
3Mydlo3 <mydlo333@interia.pl>
2020-01-25 15:14:16 +0100

    Add simulation settings
    Extracted from code, added ability to set equal green times or stream expected intervals

--- main.py
+++ main.py
@@ -8,26 +8,54 @@ from time_class import Time
     SCRIPT
 """
 
+# Simulation settings
+# How many simulations will run
+simulations_number = 100
+# How many times simulation will be run for given params (to get average result)
+simulation_repeats = 3
+# Range for green light duration (in seconds)
+green_duration_range = [15, 45]
+# Controls whether all traffic lights have the same green light duration
+equal_green_duration = True
+# Range for stream cars arrival expected interval (in seconds)
+expected_interval_range = [5, 15]
+# Controls whether all streams have the same expected interval
+equal_expected_interval = False
+
 # Simulation results list
 simulations_results = {
     'lights_enabled': [],
     'lights_disabled': []
 }
 
-for _i in range(0, 100):
-    # Simulation parameters
-    expected_interval = [np.random.randint(low=50.0, high=300.0)/10 for _i in range (0, 6)]
-    lights_enabled = True
-    green_duration = [np.random.randint(low=15, high=45) for _i in range (0, 3)]
+for _i in range(0, simulations_number):
+    # Randomize simulation parameters
+    # Green duration
+    green_duration = []
+    if equal_green_duration:
+        green_duration = np.random.randint(low=green_duration_range[0], high=green_duration_range[1])
+        green_duration = [green_duration for _i in range (0, 3)]
+    else:
+        green_duration = [np.random.randint(low=green_duration_range[0], high=green_duration_range[1]) for _i in range (0, 3)]
+
+    # Expected interval
+    expected_interval = []
+    if equal_expected_interval:
+        expected_interval = np.random.randint(low=expected_interval_range[0]*10, high=expected_interval_range[1]*10)/10
+        expected_interval = [expected_interval for _i in range (0, 3)]
+    else:
+        expected_interval = [np.random.randint(low=expected_interval_range[0]*10, high=expected_interval_range[1]*10)/10 for _i in range (0, 6)]
+
+    # Lists to store temporary results
     simulation_results_lights_enabled = []
     simulation_results_lights_disabled = []
-    # Repeat 3 times for average
-    for _j in range (0, 3):
-        # Create and run simulation
-        simulation = Simulation(expected_interval, lights_enabled, green_duration)
+    # Repeat 'simulation_repeats' times for average for given params
+    for _j in range (0, simulation_repeats):
+        # Create and run simulation with lights enabled
+        simulation = Simulation(expected_interval=expected_interval, lights_enabled=True, green_duration=green_duration)
         simulation_results_lights_enabled.append(simulation.get_results()['avg_time_in_system'].convert_to_seconds())
-        # And second time, with lights switched
-        simulation = Simulation(expected_interval, not lights_enabled, green_duration)
+        # And second time, with lights disabled
+        simulation = Simulation(expected_interval=expected_interval, lights_enabled=False, green_duration=green_duration)
         simulation_results_lights_disabled.append(simulation.get_results()['avg_time_in_system'].convert_to_seconds())
         print(f"{_i} : {_j}")
     simulations_results['lights_enabled'].append(np.average(np.array(simulation_results_lights_enabled)))
